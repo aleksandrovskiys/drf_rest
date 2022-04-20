ffd94a77a6e12699c0519f17ad7943b6d84db743
Semen Aleksandrovskii <psifisher@gmail.com>
2022-04-20 14:38:42 +0300

    chapter 1

--- demo/urls.py
+++ demo/urls.py
@@ -1,11 +1,13 @@
+import store.api_views
+import store.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-import store.views
-
 urlpatterns = [
+    path("api/v1/products/", store.api_views.ProductList.as_view()),
+
     path('admin/', admin.site.urls),
     path('products/<int:id>/', store.views.show, name='show-product'),
     path('cart/', store.views.cart, name='shopping-cart'),
